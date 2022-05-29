9ffe3b7db03a657997d02519bf5f266cf4ceff02
parkdongmin <sayg1o1d@gmail.com>
2022-05-29 22:09:59 +0900

    basic : 33

--- 33.js
+++ 33.js
@@ -9,10 +9,43 @@
 출력 : 8 7 6 4 2
  */
 
-function reversInt(s) {
-    for (let i = 0; i < s.length; i++) {
-        let result = s[i].split(" ").reverse();
+// function reversInt(s) {
+//     const result = s.split(" ").reverse().join("");
+//     // let result = "";
+//     for (let i = 0; i < s.length; i++) {
+//         result += s[i];
+//     }
+//     console.log(result);
+// }
+// reversInt(21313);
+
+// function reversInt(s) {
+//     let reversed = s.toString().split("").reverse().join("");
+//     return parseInt(reversed) * Math.sign(s);
+// }
+// reversInt(1);
+// reversInt(123456);
+// console.log(s);
+
+// function reverseInt(n) {
+//     let reversed = n.toString().split("").reverse().join("");
+//     return parseInt(reversed) * Math.sign(n);
+// }
+// reverseInt(981);
+// console.log(reversed);
+// console.log(reverseInt.n);
+
+const reverseInt = function reverseInt(s) {
+    const data = Math.abs(s).toString();
+    const result = +data
+        .split("")
+        .reverse()
+        .reduce((a, b) => a + b);
+    if (s < 0) {
+        return -result;
+    } else {
+        return result;
     }
-    console.log(result);
-}
-reversInt(12345);
+};
+console.log(reverseInt(12345));
+console.log(reverseInt(-54321));
