594f061c0675f4cb4c858021e398021baef8a588
parkdongmin <sayg1o1d@gmail.com>
2022-05-29 16:16:09 +0900

    basic : 20

---
+++ 20.js
@@ -0,0 +1,11 @@
+/*
+    공백으로 구분하여 두 숫자가 주어집니다.
+두번째 숫자로 첫번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력하세요.
+ */
+
+function sumRest(a, b) {
+    let result = a / b;
+    let remainder = a % b;
+    console.log(`result:${result} remainder:${remainder}`);
+}
+sumRest(10, 2);
