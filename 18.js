4d8bb106920043ca02b0be925edb8378fca5ed15
parkdongmin <sayg1o1d@gmail.com>
2022-05-29 15:45:37 +0900

    basic : 18

--- 18.js
+++ 18.js
@@ -10,4 +10,14 @@ function AverageScore(korean, math, english) {
     let average = sumScore / 3;
     return console.log("averageScore: " + Math.ceil(average));
 }
-AverageScore(100, 50, 50);
+AverageScore(20, 30, 40);
+//  ceil 반올림
+// floor 버림
+
+const scores = prompt("세 과목의 점수를 입력하세요.").split(" ");
+let sum = 0;
+for (let i = 0; i < 3; i++) {
+    sum += parseInt(scores[i], 10); // 십진수의 형태의 숫자로 데이터 타입을 변환합니다.
+}
+
+console.log(Math.floor(sum / 3)); //Math.floor 메서드는 소수점 자리를 모두 버림합니다.
