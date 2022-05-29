ecb1987ab006c204ac83fb9ca2dc00a552936b6e
parkdongmin <sayg1o1d@gmail.com>
2022-05-29 14:17:35 +0900

    basic : 14

---
+++ 14.js
@@ -0,0 +1,39 @@
+// var year = "2019";
+// var month = "04";
+// var day = "26";
+// var hour = "11";
+// var minute = "34";
+// var second = "27";
+
+// const result = year.concat("/");
+// month.concat("/");
+// day.concat("/");
+// (" ");
+// hour.concat(":");
+// minute.concat(":");
+// second;
+
+// // const result1 = year.concat("/", month, "/");
+// var result2 = year.concat(
+//     "/",
+//     month,
+//     "/",
+//     day,
+//     " ",
+//     hour,
+//     ":",
+//     minute,
+//     ":",
+//     second
+// );
+// console.log(result);
+// console.log(result2);
+
+function besu(s) {
+    // const result = "zza";
+    if (s % 3 == 0) {
+        console.log("zza");
+    } else console.log(s);
+}
+
+let s = besu(3);
