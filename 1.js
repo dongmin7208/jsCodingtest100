581101792a6dddb4f742a9fc815ddf1ec9cf786c
dongmin7208 <dongmin.park.career@gmail.com>
2023-05-05 00:36:45 +0900

    1.js

---
+++ 1.js
@@ -0,0 +1,8 @@
+// 다음배열에서 400, 500를 삭제하는 code를 입력하세요.
+// 次の配列から400、500を削除するcodeを入力してください。
+// Write code to delete 400 and 500 from the following array.
+var nums = [100, 200, 300, 400, 500];
+
+nums.splice(3, 2);
+
+console.log(nums);
