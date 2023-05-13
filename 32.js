9c9e9932645c5782e49ed9d071f5ff047b226555
dongmin7208 <dongmin.park.career@gmail.com>
2023-05-13 17:03:25 +0900

    ADD: 32.js

--- 32.js
+++ 32.js
@@ -1,24 +1,30 @@
-/*
-    문자열 만들기
+// //
 
-    취업 준비생인 혜림이는 자기소개서를 쓰고 있습니다. 열심히 자기소개서를 작성하던 도중 혜림이는 자기가 지금까지 단어를 얼마나 적었는지 궁금하게 됩니다. 
+const words = "hellow wolrd watasiwa park desu";
 
-    혜림이를 위해 문자열을 입력받으면 단어의 갯수를 출력하는 프로그램을 작성해 주세요.
- */
+console.log(words.split(" ").length);
 
-function moji(s) {
-    // s.includes()
-    let count = s.split(" ").length;
-    console.log(count);
-}
+// /*
+//     문자열 만들기
 
-moji("fff sdijie. cijeijc 222eji eid.");
+//     취업 준비생인 혜림이는 자기소개서를 쓰고 있습니다. 열심히 자기소개서를 작성하던 도중 혜림이는 자기가 지금까지 단어를 얼마나 적었는지 궁금하게 됩니다.
 
-function moji2(s2) {
-    let count = s2.match(/ /g).filter(function (item) {
-        return item !== "";
-    }).length;
-    console.log(count);
-}
+//     혜림이를 위해 문자열을 입력받으면 단어의 갯수를 출력하는 프로그램을 작성해 주세요.
+//  */
 
-moji2("fff sdijie. cijeijc 222eji eid.");
+// function moji(s) {
+//     // s.includes()
+//     let count = s.split(" ").length;
+//     console.log(count);
+// }
+
+// moji("fff sdijie. cijeijc 222eji eid.");
+
+// function moji2(s2) {
+//     let count = s2.match(/ /g).filter(function (item) {
+//         return item !== "";
+//     }).length;
+//     console.log(count);
+// }
+
+// moji2("fff sdijie. cijeijc 222eji eid.");
